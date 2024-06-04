a50fe71c200d9b79e2636decbd192cb4968daa62
Pacifique Hakizimana <pachakizimana@eucl.reg.rw>
2024-06-04 15:44:26 +0200

    Amazona

---
+++ frontend/src/data.js
@@ -0,0 +1,93 @@
+const data={
+    products:[
+        {
+            _id:'1',
+            name:'Nike Slim Shirt',
+            category:'Shirts',
+            image:'/images/p1.jpg',
+            price:120,
+            brand:'Nike',
+            rating:4.5,
+            numReviews:10,
+            description:'High Quality Product'
+        },
+        {
+            _id:'2',
+            name:'Adidas Slim Shirt',
+            category:'Shirts',
+            image:'/images/p1.jpg',
+            price:100,
+            brand:'Adidas',
+            rating:4.0,
+            numReviews:10,
+            description:'High Quality Product'
+        },
+        {
+            _id:'3',
+            name:'Lacoste Free Shirt',
+            category:'Shirts',
+            image:'/images/p1.jpg',
+            price:220,
+            brand:'Lacoste',
+            rating:4.8,
+            numReviews:17,
+            description:'High Quality Product'
+        },
+        {
+            _id:'4',
+            name:'Nike Slim Pant',
+            category:'Pants',
+            image:'/images/p1.jpg',
+            price:78,
+            brand:'Nike',
+            rating:4.5,
+            numReviews:10,
+            description:'High Quality Product'
+        },
+        {
+            _id:'5',
+            name:'Puma Slim Shirt',
+            category:'Shirts',
+            image:'/images/p1.jpg',
+            price:65,
+            brand:'Puma',
+            rating:4.5,
+            numReviews:10,
+            description:'High Quality Product'
+        },
+        {
+            _id:'6',
+            name:'Adidas Fit Pant',
+            category:'Pants',
+            image:'/images/p1.jpg',
+            price:139,
+            brand:'Adidas',
+            rating:4.5,
+            numReviews:15,
+            description:'High Quality Product'
+        },
+        {
+            _id:'7',
+            name:'LuisVuiton Slim Shirt',
+            category:'Shirts',
+            image:'/images/p1.jpg',
+            price:320,
+            brand:'LouisVuiton',
+            rating:4.9,
+            numReviews:19,
+            description:'High Quality Product'
+        },
+        {
+            _id:'8',
+            name:'MadeinRwanda Slim Shirt',
+            category:'Shirts',
+            image:'/images/p1.jpg',
+            price:45,
+            brand:'MadeinRwanda',
+            rating:4.5,
+            numReviews:10,
+            description:'High Quality Product'
+        }
+    ]
+}
+export default data;
